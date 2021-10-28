d8793ec93cf1efe2d0877abb3b79ae73c21bc552
rubelyarreaga <61261076+rubelyarreaga@users.noreply.github.com>
2021-10-28 12:15:42 -0600

    Delete tes/ap/home directory

--- tes/ap/home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Cliente,TipoCliente,Estudiante
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(TipoCliente)
-admin.site.register(Estudiante)
